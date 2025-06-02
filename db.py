53013e41af3573e8ede9c6bf96f4e828bc660f23
Someone142716 <antonchemisov@mail.ru>
2025-06-02 15:15:45 +0300

    Фикс
    Я очень круто умею делать неработающий код

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import sqlite3
-from aiogram import Router, F
+import asyncio
+from aiogram import Bot, Dispatcher, Router, F
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
@@ -20,16 +21,17 @@ def create_db():
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Users(
-        UserID INTEGER PRIMARY KEY AUTOINCREMENT
-        Name TEXT
+        UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Name TEXT,
         TelegramID INTEGER
     )""")
 
     cursor.execute("""
-    CREATE TAABLE IF NOT EXISTS Auth_links
-        Auth_link TEXT NOT NULL
-        UserID INTEGER NOT NULL
-        FOREIGN KEY (UserID) REFERENCES Users (UserID)""")
+    CREATE TABLE IF NOT EXISTS Auth_links(
+        Auth_link TEXT NOT NULL,
+        UserID INTEGER NOT NULL,
+        FOREIGN KEY (UserID) REFERENCES Users (UserID)
+        )""")
 
     conn.commit()
     conn.close()
@@ -39,7 +41,7 @@ def is_admin(user_id: int):
     if user_id == ADMIN_ID:
         return True
     else:
-        pass  # TODO: Считывание админки с БД
+        return True  # TODO: Считывание админки с БД
 
 
 @router.message(Command("users"))
@@ -65,9 +67,10 @@ async def add_user_cb(cb: CallbackQuery, state: FSMContext):
 @router.message(Form.name)
 async def on_name_input(message: Message, state: FSMContext):
     user_name = message.text
-    conn = sqlite3.connect("auth_bot.db")
+    conn = sqlite3.connect("auth.db")
     cursor = conn.cursor()
     try:
+        await message.answer("Все хорошо, отвечаю")
         pass  # Тут уже нужна генерация ссылки
     except Exception as err:
         print(f"Произошла ошибка при добавлении пользователя: {err}")
@@ -80,3 +83,17 @@ async def on_name_input(message: Message, state: FSMContext):
 @router.callback_query(F.data == "list_users")
 async def users_list_cb(cb: CallbackQuery):
     pass  # TODO: Сделать эту функцию
+
+
+async def main():
+    create_db()
+    bot_token = "6070749351:AAHYiHEoq40xZnGarhJNFxwjnEdEBeYUId4"
+    bot = Bot(token=bot_token)
+    dp = Dispatcher()
+    dp.include_router(router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
